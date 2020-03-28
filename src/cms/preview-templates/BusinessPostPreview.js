3f5b3016af8809f4a41bd264b285b0682640b8ca
Kristian Rogelstad <kristian@kristiandavid.com>
2020-03-28 17:34:00 -0400

    v1

---
+++ src/cms/preview-templates/BusinessPostPreview.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { BusinessPostTemplate } from '../../templates/business-post'
+
+const BusinessPostPreview = ({ entry, widgetFor }) => {
+  const tags = entry.getIn(['data', 'tags'])
+  return (
+    <BusinessPostTemplate
+      content={widgetFor('body')}
+      description={entry.getIn(['data', 'description'])}
+      tags={tags && tags.toJS()}
+      title={entry.getIn(['data', 'title'])}
+    />
+  )
+}
+
+BusinessPostPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default BusinessPostPreview
