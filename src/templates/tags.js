6e3179102740a97db9abbce3cd7b1778c8f059c1
Kristian Rogelstad <kristian@kristiandavid.com>
2020-04-04 14:41:09 -0400

    v1- update stuff

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -9,7 +9,7 @@ class TagRoute extends React.Component {
     const postLinks = posts.map(post => (
       <li key={post.node.fields.slug}>
         <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
+          <h2>{post.node.frontmatter.title}</h2>
         </Link>
       </li>
     ))
@@ -32,7 +32,7 @@ class TagRoute extends React.Component {
               >
                 <h1 className="title is-size-2 is-bold-light tagsH1">{tagHeader}</h1>
                 <p className="allTags"><Link to="/tags/">Browse all tags</Link></p>
-                <ul className="taglist">{postLinks}</ul>
+                <ul className="taglist categoriesTags">{postLinks}</ul>
               </div>
             </div>
           </div>
