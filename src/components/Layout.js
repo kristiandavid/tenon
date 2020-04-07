1cb77822c6a10e0a7aed5a16191df1be365be552
Kristian Rogelstad <kristian@kristiandavid.com>
2020-04-06 22:55:31 -0400

    v1 - og son!

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -42,7 +42,7 @@ const TemplateWrapper = ({ children }) => {
 
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
+        <meta property="og:url" content="https://rallycry.ca" />
         <meta
           property="og:image"
           content={`${withPrefix('/')}img/og-image.jpg`}
